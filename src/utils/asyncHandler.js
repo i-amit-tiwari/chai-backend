c9c106eea30af501e3ef5fef57fad5cd5e3785a1
[Amit Tiwari] <[iamit8851@gmail.com]>
2024-10-05 16:43:58 +0530

    utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req, res,  next)).catch((err) => next(err))
+    }
+ }
+
+
+
+
+export {asyncHandler}
+
+
+
+
+
+/*
+try-catch  method
+const asyncHandler = (fn) => async (error,req,res,next) => {
+    try {
+    } catch (error) {
+     res.status(err.code || 500).json({
+        success: false,
+        message: err.message})}
+    }
+    */
